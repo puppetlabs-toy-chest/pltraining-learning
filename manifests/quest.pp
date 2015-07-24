d6c4094e320b4ee465f335e74f83a4a2d0f6f92b
kjhenner <kjhenner@gmail.com>
2015-07-24 12:37:17 -0700

    add graphite vhost

--- manifests/quest.pp
+++ manifests/quest.pp
@@ -37,43 +37,43 @@ class learning::quest ($git_branch='release') {
 #  }
 #
   # Serve Graphite
-#
-#  class { 'apache':
-#    default_vhost => false,
-#  }
-#
-#  apache::vhost { "*:${graph_port}":
-#    manage_docroot => false,
-#    port           => $graph_port,
-#    docroot        => '/opt/graphite/webapp',
-#    wsgi_application_group      => '%{GLOBAL}',
-#    wsgi_daemon_process         => 'graphite',
-#    wsgi_daemon_process_options => {
-#      processes          => '5',
-#      threads            => '5',
-#      display-name       => '%{GROUP}',
-#      inactivity-timeout => '120',
-#    },
-#    wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
-#    wsgi_import_script_options  => {
-#      process-group     => 'graphite',
-#      application-group => '%{GLOBAL}'
-#    },
-#    wsgi_process_group          => 'graphite',
-#    wsgi_script_aliases         => {
-#      '/' => '/opt/graphite/conf/graphite.wsgi'
-#    },
-#    headers => [
-#      'set Access-Control-Allow-Origin "*"',
-#      'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
-#      'set Access-Control-Allow-Headers "origin, authorization, accept"',
-#    ],
-#    directories => [{
-#      path => '/media/',
-#      order => 'deny,allow',
-#      allow => 'from all'}
-#    ]
-#  }
+
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { "*:${graph_port}":
+    manage_docroot => false,
+    port           => $graph_port,
+    docroot        => '/opt/graphite/webapp',
+    wsgi_application_group      => '%{GLOBAL}',
+    wsgi_daemon_process         => 'graphite',
+    wsgi_daemon_process_options => {
+      processes          => '5',
+      threads            => '5',
+      display-name       => '%{GROUP}',
+      inactivity-timeout => '120',
+    },
+    wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
+    wsgi_import_script_options  => {
+      process-group     => 'graphite',
+      application-group => '%{GLOBAL}'
+    },
+    wsgi_process_group          => 'graphite',
+    wsgi_script_aliases         => {
+      '/' => '/opt/graphite/conf/graphite.wsgi'
+    },
+    headers => [
+      'set Access-Control-Allow-Origin "*"',
+      'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
+      'set Access-Control-Allow-Headers "origin, authorization, accept"',
+    ],
+    directories => [{
+      path => '/media/',
+      order => 'deny,allow',
+      allow => 'from all'}
+    ]
+  }
 
   # Create docroot for lvmguide files, so the website files
   # can be put in place
@@ -83,7 +83,7 @@ class learning::quest ($git_branch='release') {
   }
 
   file { $doc_root:
-    ensure  => directory,
+    ensure  => directory,directory
     owner   => 'nginx',
     group   => 'nginx',
     mode    => '755',
