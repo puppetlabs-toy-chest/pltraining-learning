4f0b198d3aac61438214a8e67e749147e9f5e499
kjhenner <kjhenner@gmail.com>
2015-08-03 12:56:46 -0700

    We've removed the examples and don't need to install them

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,15 +2,6 @@ class learning::install {
   
   $prod_module_path = '/etc/puppetlabs/code/environments/production/modules'
 
-  # Put examples in place -- we should have some way to automatically get the
-  # most recent from the puppet docs source, where they'll be in
-  # source/learning/files/examples.
-  file {'/root/examples':
-    ensure  => directory,
-    source  => "puppet:///modules/${module_name}/examples",
-    recurse => true,
-  }
-
   file {[ "$prod_module_path", 
           "${prod_module_path}/cowsayings",
           "${prod_module_path}/cowsayings/manifests",
