88de676de63acb153eaf4574cce0d7b2d39d733d
kjhenner <kjhenner@gmail.com>
2015-08-03 14:56:52 -0700

    python-sqlite3dbm should use yum provider

--- manifests/graphite_reqs.pp
+++ manifests/graphite_reqs.pp
@@ -49,7 +49,7 @@ class learning::graphite_reqs {
   }
   package { 'python-sqlite3dbm':
     ensure => '0.1.4-6.el7',
-    provider => 'pip',
+    provider => 'yum',
     require => [Package['python-pip'],Exec['/bin/pip install requests[security]']],
   }
   # Workaround for package installation target that isn't recognized by
