6f6b7ddc68a1f9f4ff6df03e381105c1ffca855a
Josh Samuelson <js@puppetlabs.com>
2015-11-11 14:02:46 -0800

    Correct pygments gem name

--- manifests/quest.pp
+++ manifests/quest.pp
@@ -110,7 +110,7 @@ class learning::quest ($git_branch='release') {
     ensure => directory,
   }
 
-  $rake_gems = ['jekyll','redcarpet','pygments']
+  $rake_gems = ['jekyll','redcarpet','pygments.rb']
   $rake_gems.each |$gem| {
     exec { "install ${gem}":
       command => "/opt/puppetlabs/puppet/bin/gem install ${gem} -i /opt/quest/gems -n /opt/quest/bin --source https://rubygems.org/",
