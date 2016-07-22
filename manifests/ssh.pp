e8b7e42b07ddec84f7b1f4c24f9d6b8747b8b98c
kjhenner <kjhenner@gmail.com>
2016-07-21 17:15:07 -0700

    disable strict host key checking and known hosts file for docker nodes

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -6,6 +6,15 @@ class learning::ssh {
     require => Package['ruby_augeas_lib'],
   }
 
+  augeas { "disable_key_checking":
+    context => '/files/etc/ssh/ssh_config',
+    changes =>
+      ["set Host[.='*.learning.puppetlabs.vm'] *.learning.puppetlabs.vm",
+       "set Host[.='*.learning.puppetlabs.vm']/StrictHostKeyChecking no",
+       "set Host[.='*.learning.puppetlabs.vm']/UserKnownHostsFile /dev/null"],
+    require => Package['ruby_augeas_lib'],
+  }
+
   service { 'sshd':
     ensure     => running,
     enable     => true,
