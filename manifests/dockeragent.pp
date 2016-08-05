916bc95711210260b49f9c931ea142bfb2777a3e
kjhenner <kjhenner@gmail.com>
2016-08-05 13:33:20 -0700

    remove persistent install of dockeragent module as a better method has been implemented in the build repo

--- manifests/dockeragent.pp
+++ manifests/dockeragent.pp
@@ -3,11 +3,4 @@ class learning::dockeragent {
   class { "dockeragent":
     create_no_agent_image => true,
   }
-  # Install the dockeragent module itself. TODO move this to a more sensible
-  # method for pre-installing modules.
-  vcsrepo { '/etc/puppetlabs/code/environments/production/modules/dockeragent':
-    ensure   => present,
-    provider => git,
-    source   => "https://github.com/puppetlabs/pltraining-dockeragent.git",
-  }  
 }
