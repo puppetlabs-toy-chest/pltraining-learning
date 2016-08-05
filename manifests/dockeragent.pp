3ed3f5429aacde29bcfeae15e117286331b4d987
kjhenner <kjhenner@gmail.com>
2016-08-05 16:06:32 -0700

    set lvm_bashrc param to true

--- manifests/dockeragent.pp
+++ manifests/dockeragent.pp
@@ -2,6 +2,7 @@ class learning::dockeragent {
   # Run the dockeragent class to pre-install images
   class { "dockeragent":
     create_no_agent_image => true,
+    lvm_bashrc            => true,
   }
   # Install the dockeragent module itself. TODO move this to a more sensible
   # method for pre-installing modules.
