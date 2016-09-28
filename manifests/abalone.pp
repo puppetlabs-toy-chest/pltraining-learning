7edf1edefbbba2713cc3a071a1e41665b75e0031
kjhenner <kjhenner@gmail.com>
2016-09-28 16:19:27 -0700

    pts through 15, remove file_line

--- manifests/abalone.pp
+++ manifests/abalone.pp
@@ -2,17 +2,11 @@ class learning::abalone {
 
   include ::abalone
 
-  range("0","9").each |Integer $number| {
+  range("0","15").each |Integer $number| {
     file_line { "securetty_pts_${number}":
       path => '/etc/securetty',
       line => "pts/${number}"
     }
-  }
-
-  file_line { "system-auth":
-    path   => '/etc/pam.d/system-auth',
-    match  => 'account\s+sufficient\s+pam_succeed_if\.so uid < 1000 quiet',
-    line   => '#account     sufficient pam_succeed_if.so uid < 1000 quiet',
   }
 
 }
