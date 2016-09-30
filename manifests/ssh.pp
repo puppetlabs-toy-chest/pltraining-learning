9ef0295a099bfa563c63e05086cca2f6ca4d2c3f
kjhenner <kjhenner@gmail.com>
2016-09-30 14:19:09 -0700

    change ssh config host name

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -9,9 +9,9 @@ class learning::ssh {
   augeas { "disable_key_checking":
     context => '/files/etc/ssh/ssh_config',
     changes =>
-      ["set Host[.='*.learning.puppetlabs.vm'] *.learning.puppetlabs.vm",
-       "set Host[.='*.learning.puppetlabs.vm']/StrictHostKeyChecking no",
-       "set Host[.='*.learning.puppetlabs.vm']/UserKnownHostsFile /dev/null"],
+      ["set Host[.='*.puppet.vm'] *.learning.puppetlabs.vm",
+       "set Host[.='*.puppet.vm']/StrictHostKeyChecking no",
+       "set Host[.='*.puppet.vm']/UserKnownHostsFile /dev/null"],
     require => Package['ruby_augeas_lib'],
   }
 
