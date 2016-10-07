22fdbf9a901587fb77e725e2698ca6f88dbb3fec
Kevin Henner <kjhenner@users.noreply.github.com>
2016-10-07 11:31:04 -0700

    Revert "remove the /etc/securetty file to permit allthettys"

--- manifests/abalone.pp
+++ manifests/abalone.pp
@@ -2,8 +2,11 @@ class learning::abalone {
 
   include ::abalone
 
-  file { '/etc/securetty':
-    ensure => absent,
+  range("0","15").each |Integer $number| {
+    file_line { "securetty_pts_${number}":
+      path => '/etc/securetty',
+      line => "pts/${number}"
+    }
   }
 
 }
