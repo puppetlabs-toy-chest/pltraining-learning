dc5b6a064f723f5a6cf62a6a6cd7da8e211dbb0a
kjhenner <kjhenner@gmail.com>
2016-10-07 13:49:55 -0700

    add bannerfile to abalone

--- manifests/abalone.pp
+++ manifests/abalone.pp
@@ -1,6 +1,8 @@
 class learning::abalone {
 
-  include ::abalone
+  class { '::abalone':
+    bannerfile => '/etc/issue',
+  }
 
   range("0","15").each |Integer $number| {
     file_line { "securetty_pts_${number}":
