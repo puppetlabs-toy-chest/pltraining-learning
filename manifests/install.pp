a124c628f87362e7a25fcc3ce0e98a7295417146
kjhenner <kjhenner@gmail.com>
2017-02-02 16:44:24 -0800

    Cleaning out graphite reqs

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,7 @@ class learning::install {
   
   $prod_module_path = '/etc/puppetlabs/code/environments/production/modules'
 
-  # Install mutli_node module
   include learning::multi_node
-  include pe_repo::platform::ubuntu_1404_amd64
 
   # Install pltraining-dockeragent module
   include docker
@@ -21,23 +19,6 @@ class learning::install {
     target  => '/usr/bin/pip',
     require => Package['python-pip']
   }
-
-  # This class sets up a local pypi server so we can run offline
-  #class { 'learning::pypi_server':
-  #  pypi_dir     => '/opt/pypiserver',
-  #  pypi_pkg_dir => '/opt/pypiserver/packages',
-  #  require      => File['/usr/bin/pip-python'],
-  #}
-
-  # Install or cache everything we need for graphite
-  #class { 'learning::graphite_reqs':
-  #  pypi_dir     => '/opt/pypiserver',
-  #  pypi_pkg_dir => '/opt/pypiserver/packages',
-  #  require      => Class['learning::pypi_server'],
-  #}
-
-  # Set up an apache server and vhost to serve graphite
-  include learning::graphite_server
 
   service {['pe-puppet', 'puppet']:
     ensure  => 'stopped',
@@ -57,21 +38,6 @@ class learning::install {
 
   file { '/var/lib/hiera':
     ensure => directory,
-  }
-
-  file { '/usr/local/bin/reset_demo':
-    ensure => 'file',
-    owner  => 'root',
-    mode   => '0500',
-    source => 'puppet:///modules/learning/reset_demo',
-  }
-
-  #enable GSSAPIAuthentication so we can disable it in the quest
-  file_line { 'sshd_config':
-    ensure => present,
-    path   => '/etc/ssh/sshd_config',
-    line   => 'GSSAPIAuthentication yes',
-    match  => '^GSSAPIAuthentication',
   }
 
 }
