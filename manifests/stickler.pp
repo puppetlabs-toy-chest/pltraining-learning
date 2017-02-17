2523103966e125b7c7a46889bdb7bd153f2011e3
kjhenner <kjhenner@gmail.com>
2017-02-17 12:45:34 -0800

    fix stickler template path

--- manifests/stickler.pp
+++ manifests/stickler.pp
@@ -7,7 +7,7 @@ class learning::stickler(
   }
   $stickler_config_hash = {'gem_dir' => $gem_dir}
   file { '/etc/systemd/system/stickler.service':
-    content => epp('stickler/stickler.service.epp', $stickler_config_hash),
+    content => epp('learning/stickler.service.epp', $stickler_config_hash),
     notify  => Service['stickler'],
   }
   service { 'stickler':
@@ -22,7 +22,7 @@ class learning::stickler(
     'server'   => 'http://localhost:6789'
   }
   file { '/root/.gem/stickler':
-    content => epp('stickler/stickler.epp', $stickler_server_hash),
+    content => epp('learning/stickler.epp', $stickler_server_hash),
     notify  => Service['stickler'],
     require => Package['stickler']
   }
